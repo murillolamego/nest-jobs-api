2cf85a2ba517d1e92e0437b7ca27765f49fed8e8
murillolamego <murillolamegolopes@live.com>
2024-05-20 11:07:54 -0300

    documentation via swagger and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,64 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+interface ExpressSwaggerCustomOptions {
+  explorer?: boolean;
+  swaggerOptions?: Record<string, any>;
+  customCss?: string;
+  customCssUrl?: string;
+  customJs?: string;
+  customfavIcon?: string;
+  swaggerUrl?: string;
+  customSiteTitle?: string;
+  validatorUrl?: string;
+  url?: string;
+  urls?: Record<'url' | 'name', string>[];
+}
+
+const customStyle = `
+.swagger-ui .topbar { display: none } 
+.swagger-ui .information-container{background-color: #636C7A; max-width: 100%; padding: 40px 30px 20px} 
+.swagger-ui .info {margin: 0} 
+.swagger-ui .info .title{color: #F7F9FD}
+.swagger-ui .btn {box-shadow: none}
+.swagger-ui .btn.authorize {background-color: #636C7A; color: #F7F9FD; border-color: #636C7A}
+.swagger-ui .btn.authorize svg{fill: #F7F9FD}
+.swagger-ui .authorization__btn svg{fill: #636C7A}`;
+
+const expressSwaggerCustomOptions: ExpressSwaggerCustomOptions = {
+  swaggerOptions: {
+    tagsSorter: 'alpha',
+  },
+  customCss: customStyle,
+  customSiteTitle: 'MeuLar API',
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Jobs API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('v1');
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(
+    '/documentation/v1',
+    app,
+    document,
+    expressSwaggerCustomOptions,
+  );
+
   const port = configService.get<string>('PORT');
   await app.listen(port);
   console.log(`Server listening on port ${port}`);
